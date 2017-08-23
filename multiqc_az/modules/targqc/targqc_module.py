03676abcd422a7ba7684998a457b1979dde56b40
vladsaveliev <vladsaveliev@me.com>
2017-08-23 16:24:20 +0300

    Fix py2 divisions

--- multiqc_az/modules/targqc/targqc_module.py
+++ multiqc_az/modules/targqc/targqc_module.py
@@ -2,6 +2,7 @@
 
 """ MultiQC module to parse output from TargQC """
 
+from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 from collections import defaultdict, OrderedDict
